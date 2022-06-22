edd2e515b261ef88c813eb28ef8bb71a087e924e
priyanka <juyal.priyanka94@gmail.com>
2022-06-22 14:58:13 +0530

    Added echo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//I jus adding line
+		//I just adding line
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Ashutosh ullu hai");
 	}
