977d2ca60e52de3fd713507ad2b68fd0e16bb794
priyankaj3w <juyal.priyanka94@gmail.com>
2022-06-22 20:54:04 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		//I just adding line
-		SpingApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Ashutosh ullu hai");
 	}
 
